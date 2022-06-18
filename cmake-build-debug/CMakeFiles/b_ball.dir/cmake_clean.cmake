99af96f6bb6165f5f2a38804a13c94d517ff8c6a
barakdf <barakdf@gmail.com>
2022-06-18 23:29:05 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/b_ball.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/b_ball.dir/main.cpp.o"
+  "b_ball"
+  "b_ball.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/b_ball.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
